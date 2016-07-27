a096606b91834e04f7cc4f80b6f356f0f2da7edd
Istvan Szukacs <istvan@streambrightdata.com>
2016-07-27 20:18:49 +0200

    Hello #2

---
+++ run.py2.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+#unix
+virtualenv --version | egrep -q '[0-9]+\.[0-9]+\.[0-9]+' && echo 'virtualenv is installed...' || sudo pip install virtualenv virtualenvwrapper
+if [ -e py2-env/.Python ]; then 
+  echo "py2-env virtualenv is already created"
+else
+  virtualenv py2-env
+fi
+source py2-env/bin/activate
+#pip install #--upgrade pip
+pip install ipython numpy jupyter statistics matplotlib pandas seaborn scipy scikit-learn sympy plotly wordcloud pillow mlxtend SPARQLWrapper graphviz statsmodels patsy #--upgrade
+#pip install https://storage.googleapis.com/tensorflow/mac/tensorflow-0.8.0-py2-none-any.whl
+jupyter notebook
